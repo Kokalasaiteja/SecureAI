cb9fdd751dc27c410f263f39497daab66799d826
Sai Teja <saiteja.kokala@engro.io>
2026-06-23 18:51:19 +0530

    Fix: add STATIC_ROOT, WhiteNoise middleware, downgrade xgboost to avoid GPU deps

--- Usage_of_AI_in_Prevention_of_Social_Engineering_Attack/settings.py
+++ Usage_of_AI_in_Prevention_of_Social_Engineering_Attack/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Serve static files on Render
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,11 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Required for collectstatic on Render
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# WhiteNoise compressed static file storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Only include if the static/ folder actually exists locally
+import os as _os
+_static_dir = BASE_DIR / 'static'
+if _os.path.isdir(_static_dir):
+    STATICFILES_DIRS = [_static_dir]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
