2ab32e2ae110bd4091585b417feff03ac5391d50
Sai Teja <saiteja.kokala@engro.io>
2026-06-23 19:47:10 +0530

    Fix: move bg_color blocks outside content, serve media in prod

--- Usage_of_AI_in_Prevention_of_Social_Engineering_Attack/urls.py
+++ Usage_of_AI_in_Prevention_of_Social_Engineering_Attack/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from users import views
 
 urlpatterns = [
@@ -45,4 +46,5 @@ urlpatterns = [
     path('email-phishing/', views.email_phishing_analyzer, name='email_phishing_analyzer'),
     path('introduction/', views.introduction, name='introduction'),
     path('contact/', views.contact, name='contact'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
